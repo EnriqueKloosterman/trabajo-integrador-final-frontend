cf349116ab407eaaa0578ce012d29561843ac993
EnriqueKloosterman <kloostermanen@gmail.com>
2024-04-07 11:39:16 -0300

    creacíon del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>Bienvenidos una ves más al mundo del survival horror!</h2>
+      <h3>Buena Suerte!!!</h3>
+    </>
+  )
+}
+
+export default App
