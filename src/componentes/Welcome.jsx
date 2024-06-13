3a63ec79c65a4093089afb5b19cc22b9a2c6892e
Malvina14 <malvinapacheco61rgmail.com>
2024-06-13 19:47:37 -0300

    componente welcome completo

--- src/componentes/Welcome.jsx
+++ src/componentes/Welcome.jsx
@@ -1,18 +1,27 @@
-
+import React from 'react';
+import aventurasImage from '../assets/aventuras-2.jpg'; // Ajusta la ruta según donde almacenes la imagen
 
 function Welcome() {
   return (
-    <section className="welcome p-6 bg-gray-100 mt-5">
-      <h2 className="text-xl font-bold mb-4">Bienvenidos a "Aventuras en la Cocina"</h2>
-      <p className="mb-4">
-        ¡Hola a todos y bienvenidos a "Aventuras en la Cocina"! En este espacio creado con amor y dedicación para todos los amantes de la cocina, 
-        desde principiantes hasta expertos. Queremos compartir con ustedes la pasión y el gozo que 
-        hemos encontrado en el arte culinario. Aquí, todos tenemos una cosa en común: el amor
-         por la buena comida y las experiencias inolvidables que nacen en la cocina...
-      </p>
-      <a href="#" className="text-blue-500">Sigue leyendo...</a>
+    <section className="welcome p-6 bg-gray-100 rounded-md shadow-md">
+      <div className="flex flex-col md:flex-row items-center md:items-start">
+        <div className="md:w-1/3 mb-4 md:mb-0">
+          <img src={aventurasImage} alt="Aventuras en la Cocina" className="w-full rounded-md shadow-md" />
+        </div>
+        <div className="md:w-2/3 text-center md:text-left md:ml-4">
+          <h2 className="text-xl font-bold mb-4">Bienvenidos a "Aventuras en la Cocina"</h2>
+          <p className="mb-4">
+            ¡Hola a todos y bienvenidos a "Aventuras en la Cocina"! En este espacio creado con amor y dedicación para todos los amantes de la cocina, 
+            desde principiantes hasta expertos. Queremos compartir con ustedes la pasión y el gozo que 
+            hemos encontrado en el arte culinario. Aquí, todos tenemos una cosa en común: el amor
+            por la buena comida y las experiencias inolvidables que nacen en la cocina...
+          </p>
+         
+        </div>
+      </div>
     </section>
   );
 }
 
 export default Welcome;
+
