04a1abdba31946d28a3790afa8e954353a342847
Malvina14 <malvinapacheco61rgmail.com>
2024-06-14 16:32:46 -0300

    cambios en home

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Menu from './Menu';
 import Welcome from './Welcome';
 import RecipeCarousel from './RecipeCarousel';
+import Article from './article';
+import Recipe from './recipe';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <RecipeCarousel />
       <Menu />
       <Welcome />
+      <Recipe />
+      <Article />
     </div>
   );
 };
