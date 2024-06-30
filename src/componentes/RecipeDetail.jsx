cec60e60edf89bd760842d09242cdd6fb5f657fc
Florencia Da Rosa <floordarosa2002gmail.com>
2024-06-30 15:43:24 -0300

    se agregan cambios de color

--- src/componentes/RecipeDetail.jsx
+++ src/componentes/RecipeDetail.jsx
@@ -4,7 +4,6 @@ import { UserContext } from "./UserContext";
 import Comments from "./Comments";
 import EditButton from "./EditButton";
 
-
 function RecipeDetail() {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
@@ -32,23 +31,23 @@ function RecipeDetail() {
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover" />
           <div className="p-6">
-            <h2 className="text-4xl font-semibold text-green-800 mb-4">{recipe.title}</h2>
+            <h2 className="text-4xl font-semibold text-black mb-4">{recipe.title}</h2>
             <p className="text-m mb-4 italic">{recipe.user.userName} {recipe.user.userLastName} <span className="font-semibold"> {recipe.user.userEmail}</span></p>
             <div className="mb-6">
-              <h3 className="text-2xl font-semibold text-green-700 mb-2">Descripción:</h3>
+              <h3 className="text-2xl font-semibold text-black mb-2">Descripción:</h3>
               {recipe.description.map((paragraph, i) => (
                 <p key={i} className="text-gray-800 text-base">{paragraph}</p>
               ))}
             </div>
             <div className="mb-6">
-              <h3 className="text-2xl font-semibold text-green-700 mb-4">Ingredientes:</h3>
+              <h3 className="text-2xl font-semibold text-black mb-4">Ingredientes:</h3>
               <ul>
                 {recipe.ingredients.map((ingredient, i) => (
                   <li key={i} className="text-gray-800 text-base mb-4">{ingredient}</li>
                 ))}
               </ul>
               <ul>
-              <h3 className="text-2xl font-semibold text-green-700 mb-4">Instrucciones:</h3>
+              <h3 className="text-2xl font-semibold text-black mb-4">Instrucciones:</h3>
                 {recipe.instructions.map((recipe, i) => (
                   <li key={i} className="text-gray-800 text-base mb-2">{recipe}</li>
                 ))}
@@ -60,7 +59,7 @@ function RecipeDetail() {
             </div>
             <div className="text-center">
               <Link to="/recipes">
-                <button className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-green-600 active:bg-green-800">
+                <button className="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-800 active:bg-gray-700">
                   Volver a recetas
                 </button>
               </Link>
