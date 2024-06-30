794cadcf360ab63bee1cbceb9db1ae058fd1b90b
Florencia Da Rosa <floordarosa2002gmail.com>
2024-06-30 16:46:32 -0300

    se agregan cambios en home

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,16 +1,34 @@
+import React from 'react';
 import Menu from './Menu';
 import Welcome from './Welcome';
 import RecipeCarousel from './RecipeCarousel';
 import Article from './article';
-
+import Recipe from './recipe';
 
 const Home = () => {
   return (
-    <div>
+    <div className="container mx-auto p-4 min-h-screen">
       <RecipeCarousel />
       <Menu />
       <Welcome />
-      <Article />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
+        <div className="col-span-2">
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <div className="p-6">
+              <h2 className="text-3xl font-bold mb-4 text-center text-black">Recetas Populares</h2>
+              <Recipe />
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="bg-white rounded-lg shadow-md overflow-hidden">
+            <div className="p-6">
+              <h2 className="text-3xl font-bold mb-4 text-center text-black">Últimos Artículos</h2>
+              <Article />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/componentes/article.jsx
+++ src/componentes/article.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Article() {
@@ -23,50 +23,34 @@ function Article() {
   }, [searchTerm, articles]);
 
   return (
-    <div className="container mx-auto p-4 bg-blue-50 min-h-screen">
-      <div className="mb-4 flex items-center">
-        <input
-          type="text"
-          placeholder="Buscar artículos..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full px-4 py-2 border rounded-l-lg focus:outline-none focus:border-blue-500"
-        />
-        <button
-          onClick={() => setSearchTerm("")}
-          className="py-2 px-4 bg-gray-800 border bg-gray-800 text-white rounded-r-lg hover:bg-blue-600 transition-colors duration-300"
+    <div className="grid grid-cols-1 gap-4">
+      {filteredArticles.map((article, i) => (
+        <div
+          key={i}
+          className="bg-white shadow-md rounded-lg overflow-hidden article-card"
         >
-          Limpiar
-        </button>
-      </div>
-      <div className="flex-col">
-        {filteredArticles.map((article, i) => (
-          <div
-            key={i}
-            className="bg-white shadow-md rounded-lg overflow-hidden article-card"
-          >
-            <Link to={`/articles/${article.articleId}`}>
-              <img
-                src={article.image}
-                alt={article.title}
-                className="w-1/3 h-48 ml-5 object-cover"
-              />
-              <div className="p-4">
-                <h2 className="text-2xl font-semibold text-blue-700 mb-2">
-                  {article.title}
-                </h2>
-                <div className="pl-6">
-                  {article.article.slice(0, 1).map((paragraph, j) => (
-                    <p key={j} className="text-gray-700 text-base mb-2">
-                      {paragraph}
-                    </p>
-                  ))}
-                </div>
+          <Link to={`/articles/${article.articleId}`} className="block group">
+            <img
+              src={article.image}
+              alt={article.title}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold text-blue-700 mb-2">
+                {article.title}
+              </h2>
+              <div className="pl-6">
+                {article.article.slice(0, 1).map((paragraph, j) => (
+                  <p key={j} className="text-gray-700 text-base mb-2">
+                    {paragraph.slice(0, 100)} {/* Mostrar solo los primeros 100 caracteres */}
+                    {paragraph.length > 100 && "..."} {/* Añadir puntos suspensivos si el texto es más largo */}
+                  </p>
+                ))}
               </div>
-            </Link>
-          </div>
-        ))}
-      </div>
+            </div>
+          </Link>
+        </div>
+      ))}
       <div className="text-center mt-8">
         <Link to="/create-article">
           <button className="text-white bg-gray-800 hover:bg-gray-700 font-semibold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-800 active:bg-gray-900">
