e3b6055ae4db94dcd723ee08a4e2fc6089b2090d
Florencia Da Rosa <floordarosa2002gmail.com>
2024-06-30 15:48:34 -0300

    se agregan cabio de pocicion a recipe

--- src/componentes/recipe.jsx
+++ src/componentes/recipe.jsx
@@ -4,63 +4,72 @@ import { Link } from "react-router-dom";
 function Recipe() {
   const [recipes, setRecipes] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
   const recipesUrl = "http://localhost:3030/api/v2/recipes/recipes";
 
   useEffect(() => {
     fetch(recipesUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setRecipes(data);
       });
   }, []);
 
   useEffect(() => {
-    const filteredRecipes = recipes.filter((recipe) =>
+    const filtered = recipes.filter((recipe) =>
       recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    setSearchResults(filteredRecipes);
+    setFilteredRecipes(filtered);
   }, [searchTerm, recipes]);
 
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
   return (
-    <div className="container mx-auto p-4">
-      <div className="mb-4">
+    <div className="container mx-auto p-4 bg-blue-50 min-h-screen">
+      <div className="mb-4 flex items-center">
         <input
           type="text"
           placeholder="Buscar recetas..."
           value={searchTerm}
-          onChange={handleSearchChange}
-          className="px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:border-blue-500 transition duration-200"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full px-4 py-2 border rounded-l-lg focus:outline-none focus:border-blue-500"
         />
+        <button
+          onClick={() => setSearchTerm("")}
+          className="py-2 px-4 bg-gray-800 text-white rounded-r-lg hover:bg-blue-600 transition-colors duration-300"
+        >
+          Limpiar
+        </button>
       </div>
-      <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {searchResults.map((recipe, i) => (
-          <div className="recipe-card" key={i}>
-            <Link to={`/recipes/${recipe.recipeId}`} className="block group">
-              <li className="bg-white shadow-md rounded-lg overflow-hidden h-full transition transform hover:scale-105 hover:shadow-lg duration-200 ease-in-out">
-                <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h2 className="text-2xl font-semibold text-blue-900 mb-2 group-hover:text-blue-700 transition duration-200">
-                    {recipe.title}
-                  </h2>
+      <div className="flex-col">
+        {filteredRecipes.map((recipe, i) => (
+          <div
+            key={i}
+            className="bg-white shadow-md rounded-lg overflow-hidden article-card"
+          >
+            <Link to={`/recipes/${recipe.recipeId}`}>
+              <img
+                src={recipe.image}
+                alt={recipe.title}
+                className="w-1/3 h-48 ml-5 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-2xl font-semibold text-blue-700 mb-2">
+                  {recipe.title}
+                </h2>
+                <div className="pl-6">
+                  {recipe.description.slice(0, 1).map((paragraph, j) => (
+                    <p key={j} className="text-gray-700 text-base mb-2">
+                      {paragraph}
+                    </p>
+                  ))}
                 </div>
-              </li>
+              </div>
             </Link>
           </div>
         ))}
-      </ul>
+      </div>
       <div className="text-center mt-8">
         <Link to="/create-recipe">
-          <button className="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue active:bg-gray-900 transition duration-150 ease-in-out shadow-md hover:shadow-lg">
+          <button className="text-white bg-gray-800 hover:bg-gray-700 font-semibold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-800 active:bg-gray-900">
             Crear nueva receta
           </button>
         </Link>
