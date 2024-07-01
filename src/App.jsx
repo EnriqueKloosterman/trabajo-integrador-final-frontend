eca3151334b25c81b8a7e6445f9610dd2a41821d
Malvina14 <malvinapacheco61rgmail.com>
2024-07-01 16:12:56 -0300

    rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Article from "./componentes/article";
-import Comments from "./componentes/Comments"
+import Comments from "./componentes/Comments";
 import Recipe from "./componentes/recipe";
 import Users from "./componentes/Users";
 import Navbar from "./componentes/Navbar";
@@ -19,35 +19,37 @@ import Home from "./componentes/Home";
 import ProtectedRoute from './auth/ProtectedRoute';
 import Footer from "./componentes/footer";
 import UpdateRecipe from "./componentes/recipe/UpdateRecipe";
-import UpdateArticle from "./componentes/article/UpdateArticle";
 
 function App() {
   return (
     <div className="container px-3 bg-slate-100 mx-auto">
       <Navbar />
       <Routes>
-        <Route path="/recipes" element={<Recipe />} />
-        <Route path="recipes/:id" element={<RecipeDetail />} />
-        <Route path="/create-recipe" element={<CreateRecipe />} />
-        <Route path="/footer" element={< Footer />} />
-        <Route path="/users" element={<Users />} />
-        <Route exact path="/articles" element={<Article />} />
+        {/* Rutas públicas */}
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<Article />} />
         <Route path="/articles/:id" element={<ArticleDetail />} />
-        <Route path="/create-article" element={<CreateArticle />} />
+        <Route path="/recipes" element={<Recipe />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/comments" element={<Comments />} />
         <Route path="/welcome" element={<Welcome />} />
-        <Route element={<ProtectedRoute />}></Route>
-        <Route path="/menu" element={<Menu />} />
-        <Route element={<ProtectedRoute />}></Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/update/recipe/:id" element={< UpdateRecipe />} />
-        <Route path="/update/article/:id" element={< UpdateArticle />} />
+
+        {/* Rutas privadas */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/create-article" element={<CreateArticle />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/update/recipe/:id" element={<UpdateRecipe />} />
+        </Route>
+        
+        {/* Ruta de error 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,15 +1,33 @@
 import React, { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { UserContext } from '../componentes/UserContext'; // Asegúrate de ajustar la ruta según la ubicación exacta de tu UserContext
+import PropTypes from 'prop-types';
+import { UserContext } from '../componentes/UserContext';
 
 function ProtectedRoute() {
-  const { user } = useContext(UserContext); // Usa correctamente useContext para obtener el contexto
+  const { user } = useContext(UserContext);
 
   if (!user) {
-    return <Navigate to="/login" />; // Redirige al inicio de sesión si el usuario no está autenticado
-  } else {
-    return <Outlet />; // Renderiza las rutas secundarias anidadas
+    return <Navigate to="/login" />;
   }
+
+  return <Outlet />;
 }
 
+ProtectedRoute.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    isAdmin: PropTypes.bool,
+  }),
+  admin: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+  }),
+};
+
 export default ProtectedRoute;
+
+
